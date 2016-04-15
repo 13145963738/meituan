9108b7d6029e34fc34f7cc861b7f332e04e81bb6
xmg <xmg@xmgdemac-mini-2.local>
2016-04-15 10:41:20 +0800

    初始化操作

---
+++ meituan/meituan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  meituan
+//
+//  Created by xmg on 16/4/15.
+//  Copyright © 2016年 haibo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
