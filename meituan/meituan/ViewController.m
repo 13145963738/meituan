28a21c3fd988df2f7ef7785f6dcb1eb1e6746132
xmg <xmg@xmgdemac-mini-2.local>
2016-04-15 10:49:35 +0800

    更改viewdidload

--- meituan/meituan/ViewController.m
+++ meituan/meituan/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"只是一个测试而已");
 }
 
 - (void)didReceiveMemoryWarning {
