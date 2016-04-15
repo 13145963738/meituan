c5d832b5cf90f88d11b970f963462fbc1f43eb9b
xmg <xmg@xmgdemac-mini-2.local>
2016-04-15 10:51:32 +0800

    创建一个person类

---
+++ meituan/meituan/person.h
@@ -0,0 +1,13 @@
+//
+//  person.h
+//  meituan
+//
+//  Created by xmg on 16/4/15.
+//  Copyright © 2016年 haibo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface person : NSObject
+
+@end
---
+++ meituan/meituan/person.m
@@ -0,0 +1,13 @@
+//
+//  person.m
+//  meituan
+//
+//  Created by xmg on 16/4/15.
+//  Copyright © 2016年 haibo. All rights reserved.
+//
+
+#import "person.h"
+
+@implementation person
+
+@end
